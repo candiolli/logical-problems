ac32bd54d1e6f4482c3ca722b8d7069ab5b91755
scandiolli <silas.candiolli@dental-wings.com>
2023-11-30 09:58:53 -0500

    adding maven structure

---
+++ roche/src/main/java/com/silascandiolli/App.java
@@ -0,0 +1,13 @@
+package com.silascandiolli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
